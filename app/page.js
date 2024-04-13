b1753a1928eefc94c0f6bab4da48d3fe424ce789
TarunSamala <tarunsamala.6435@gmail.com>
2024-04-14 00:42:24 +0530

    Change of Plans

--- app/page.js
+++ app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 import NavBar from "@/components/NavBar";
+import List from "@/components/Listpage";
 
 
 
@@ -13,7 +14,7 @@ export default function Home() {
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <div className="col-span-7 place-self-center text-center sm:text-left">
       <h1 className="text-white mb-4 text-4xl sm:text-4xl lg:text-6xl font-extrabold ">
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-900 via-blue-500 to-pink-300">Easy Carbon: <br></br>
+        <span className="text-transparent bg-clip-text bg-gradient-to-r text-white"><span className="text-yellow-400">Easy Carbon:</span> <br></br>
         Revolutionizing <br>
         </br> Environmental <br>
         </br>
@@ -22,23 +23,24 @@ export default function Home() {
       <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl pt-10 ">
       Unlock the power of verified carbon credits with our innovative tokenization engine. Seamlessly manage your climate action journey through secure user accounts, transparent pricing, and compliant trading.
       </p>
-      <div>
-        <button className="px-6 py-5 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-700 via-pink-300 to-pink-500 hover:bg-slate-200 text-black">Get Started</button>
+      <div className="pt-10">
+        <button className="px-6 py-5 w-full sm:w-fit rounded-md mr-4 bg-gradient-to-br from-yellow-400 via-yellow-300 to-yellow-500 hover:bg-slate-200 text-black">Get Started</button>
+        <button className="px-6 py-5 w-full sm:w-fit rounded-md mr-4 bg-gradient-to-br bg-black text-white border-white bg-blend-normal">Learn More</button>
       </div>
       </div>
       <div className="col-span-5 place-self-center mt-4 lg:mt-0">
-        <div className="rounded-full bg-[#181818] w-100 h-500">
+        <div className="">
       <Image
-       src = "/Image/hero-image.png"
+       src = "/Image/hero-navi.jpg"
        alt = "hero image"
-       width={500}
-       height={500}
+       width={1000}
+       height={0}
       />
       </div>
       </div>
       </div>
       <div className="pt-10">
-        
+        <Listpage/>
       </div>
       </div>
     </main>
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -8,12 +8,12 @@ import Image from "next/image";
 
 const navLinks = [
   {
-    title: "About",
-    path: "#about",
+    title: "About Us",
+    path: "#aboutus",
   },
   {
-    title: "Buy",
-    path: "#projects",
+    title: "Solutions",
+    path: "#solutions",
   },
   {
     title:"Sell",
@@ -27,16 +27,17 @@ const navLinks = [
 const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
+  
   return (
     <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
       <div className="flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2">
       <Link href = {"/"}>
-            {/* <Image
-            src = "/Image/osc.png"
+            <Image
+            src = "/Image/EasyCarbonLogoFinal.png"
             alt = "logo"
             width={100}
             height={100}
-            /> */}EasyCarbon
+            />
           </Link>
         <div className="mobile-menu block md:hidden">
           {!navbarOpen ? (
